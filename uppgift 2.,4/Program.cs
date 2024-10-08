be95096519e0dbb634f27f20171dd704a9995861
ryhu06001 <ryhu06001@E-PW05CGRC>
2024-10-08 18:21:41 +0200

    Add project files.

---
+++ uppgift 2.,4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" hur mycket tjänar den första anställda?");
+            int tal1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("hur mycket tjänar den andra anställda?");
+            int tal2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("hur mycket tjänar den tredje anställda?");
+            int tal3 = int.Parse(Console.ReadLine());
+            int summa = tal1 + tal2 + tal3;
+            Console.WriteLine("första anställd" + summa + "andra anställd" + summa + "tredje anställd");
+        }
+    }
+}
+
+
